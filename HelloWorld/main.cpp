a006f3acafd776bec4f17a448173123ef06f604d
Kevin Day <kevin.ch.day@gmail.com>
2021-01-23 15:33:23 -0600

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// function prototypes
+void helloWorld();
+
+// main()
+int main(){
+    system("color 0a");
+    helloWorld();
+    system("pause");
+    return 0;
+}
+
+// helloWorld()
+void helloWorld(){
+    cout << "Hello world!" << endl;
+}
